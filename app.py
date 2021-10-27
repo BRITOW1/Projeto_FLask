a3027efcd2f45615f615f213262f0ef7cbc746b3
GurenLagann <dragonwits@gmail.com>
2021-10-27 15:45:21 -0300

    Second Page Create

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def login():
     return render_template('loign.html')
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
