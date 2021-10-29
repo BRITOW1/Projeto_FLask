dba0638ccb9cced50d02cadf7a72978782fd7876
salonsoWhrl <93144351+salonsoWhrl@users.noreply.github.com>
2021-10-29 10:21:05 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand, migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
+
+lm = LoginManager()
+lm.init_app(app)
+
+
+from app.models import tables
+from app.controllers import default
+
+
